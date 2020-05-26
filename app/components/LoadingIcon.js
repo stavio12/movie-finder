aa20d7eec042caa13ffb36fe35737b0cfe9e36d4
Gustave Anthony Dzoumedor <dzoumedor13@gmail.com>
2020-05-26 02:13:33 +0000

    Added Loading Icon

---
+++ app/components/LoadingIcon.js
@@ -0,0 +1,13 @@
+import React, { useEffect } from "react"
+
+function LoadingIcon() {
+  return (
+    <>
+      <div className="spinner-grow text-info" role="status">
+  <span className="sr-only">Loading...</span>
+</div>
+    </>
+  )
+}
+
+export default LoadingIcon
