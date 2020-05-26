ea47d69a93bfa8bec99c0316fabe20d0fa99b90c
Gustave Anthony Dzoumedor <dzoumedor13@gmail.com>
2020-05-26 02:25:45 +0000

    Updated and Added New Features

--- app/components/ResultCard.js
+++ app/components/ResultCard.js
@@ -8,7 +8,6 @@ function ResultCard(props) {
 
   return (
     <>
-
     <div className="container">
       <div className="row justify-content-center">
         <div className="col-12 col-md-6 py-5 ">
