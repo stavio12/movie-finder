efbbc7def03072de0f080b7cb588012eb1bcdc06
Gustave Anthony Dzoumedor <dzoumedor13@gmail.com>
2020-05-26 02:12:45 +0000

    Added Error Handler

---
+++ app/components/Error.js
@@ -0,0 +1,13 @@
+import React, { useEffect } from "react";
+
+function Error() {
+  return (
+    <>
+      <div className="alert alert-warning text-center mt-5" role="alert">
+        <h3> No Movie Found!</h3>
+      </div>
+    </>
+  );
+}
+
+export default Error;
