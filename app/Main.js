8995357262469bfab0627843926aee34617b4657
Gustave Anthony Zoumedor <dzoumedor13@gmail.com>
2020-11-20 16:58:18 +0000

    Added .env

--- app/Main.js
+++ app/Main.js
@@ -14,7 +14,7 @@ function App(props) {
     setTitle(data);
 
     try {
-      const response = await Axios.get(`YOUR KEY=${data}`);
+      const response = await Axios.get(`http://www.omdbapi.com/?i=tt3896198&apikey=${process.env.UrApi}&s=${data}`);
       if (response.data.Response == "False") {
         setNoMovie(true);
         setSearchData([]);
